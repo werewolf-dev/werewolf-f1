2dc312f6a80deccc25d19704f92dcb8514769f1d
werewolf <wezydata@gmail.com>
2025-02-26 12:41:00 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+Config.menudeolacakararaclar = {"adder", "t20"} -- Menude çıkacak araçlar değişebilirsiniz
+Config.spawncooldown = 10 -- araç cooldown'u kaç saniyede bir çıkarabilecek diye
---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Werewolf'
+description 'F1 Araç Çıkarma Menüsü'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+files {
+    'html/index.html'
+}
+
+ui_page 'html/index.html'
